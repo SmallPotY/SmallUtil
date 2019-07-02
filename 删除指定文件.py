1d7ee5a6955bb9f4ee64be404eb48092f6f943f8
SmallPotY <1041132457@qq.com>
2019-07-02 15:43:10 +0800

    删除指定文件

---
+++ 删除指定文件.py
@@ -0,0 +1,17 @@
+import os
+
+
+def get_file(file_dir):
+    lists = []
+    for root,dirs,files in os.walk(file_dir):
+        for file in files:
+            if os.path.splitext(file)[1] not in ['.py','.pyc']:
+                lists.append(os.path.basename(file))
+    return lists
+
+
+
+files = get_file(r'/home/HR/web/static/tmp')
+
+for file in files:
+    os.remove('/home/HR/web/static/tmp/' + file)
